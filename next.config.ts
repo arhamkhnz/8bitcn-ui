a9170cc33b407aaa71d4714be5f9719761ab79c1
OrcDev <urosmiric88@gmail.com>
2025-08-16 10:49:32 +0200

    feat(next.config): add remote image pattern for Twitter in Next.js configuration (#321)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ["@sparticuz/chromium", "puppeteer-core"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "pbs.twimg.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
